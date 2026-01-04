22dc16c76dd9529a23e74d2c59de4cd86acba05b
Kiss Ákos <kissakos2002@mailbox.unideb.hu>
2026-01-04 15:25:09 +0100

    Fixed paging error filters not applying

--- src/api/movies/fetchFilteredMoviesPerPage.tsx
+++ src/api/movies/fetchFilteredMoviesPerPage.tsx
@@ -2,13 +2,23 @@ import {moviesType} from "../../types.tsx";
 import axios from "axios";
 
 
-export const fetchFilteredMoviesPerPage = async (page: number, setTotalPages: React.Dispatch<React.SetStateAction<number>>, sortBy: string,
-    setFilteredData: React.Dispatch<React.SetStateAction<moviesType[]>>
+export const fetchFilteredMoviesPerPage = async (
+    page: number,
+    setTotalPages: React.Dispatch<React.SetStateAction<number>>,
+    sortBy: string,
+    setFilteredData: React.Dispatch<React.SetStateAction<moviesType[]>>,
+    category: number[],
+    releaseDate: string | undefined
 ) =>{
     try {
-        const resp = await axios.get(
-            `https://api.themoviedb.org/3/discover/movie?page=${page}&sort_by=${sortBy}&api_key=${import.meta.env.VITE_TMDB_APIKEY}`
-        );
+        let url = `https://api.themoviedb.org/3/discover/movie?page=${page}&sort_by=${sortBy}&api_key=${import.meta.env.VITE_TMDB_APIKEY}`;
+        if (releaseDate) {
+            url += `&release_date.lte=${releaseDate}`;
+        }
+        if (category.length > 0) {
+            url += `&with_genres=${category.join(",")}`;
+        }
+        const resp = await axios.get(url)
         setFilteredData(resp.data.results);
         setTotalPages(resp.data.total_pages);
         return resp.data.results || [];
