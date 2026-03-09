4c43e630d5fd4b23aee94d036be748a88fb6fdf9
Kiss Ákos <kissakos2002@mailbox.unideb.hu>
2026-03-09 14:56:27 +0100

    Performance update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import './App.scss'
 import {Navigate, Route, Routes} from "react-router"
-import HomePage from "./pages/home/HomePage.tsx"
-import MoviesPage from "./pages/movies/MoviesPage.tsx"
-import SeriesPage from "./pages/series/SeriesPage.tsx"
-import Register from "./pages/auth/Register.tsx"
-import MoviePlayer from "./pages/movies/MoviePlayer.tsx"
-import SeriesPlayer from "./pages/series/SeriesPlayer.tsx"
-import { useState} from "react"
+import {lazy, Suspense, useState} from "react"
 import { Genre} from "./types.tsx"
-import MainPage from "./pages/home/MainPage.tsx"
 import {fetchUser} from "./api/auth/fetchUser.tsx"
 import ProtectedRoute from "./pages/auth/ProtectedRoute.tsx"
 import {useQuery} from "@tanstack/react-query"
-import Watchlist from "./pages/bookmarked/Watchlist.tsx"
-import ChangePassword from "./pages/auth/ChangePassword.tsx"
 import {fetchSession} from "./api/auth/fetchSession.tsx"
-import RecoverPasswordByEmail from "./pages/auth/RecoverPasswordByEmail.tsx"
-import RecoverPassword from "./pages/auth/RecoverPassword.tsx"
-import SearchPage from "./pages/search/SearchPage.tsx";
+import {fetchPopularData} from "./api/moviesandseries/fetchPopularData.tsx";
 
+const MainPage = lazy(() => import("./pages/home/MainPage.tsx"))
+const HomePage = lazy(() => import("./pages/home/HomePage.tsx"))
+const MoviesPage = lazy(() => import("./pages/movies/MoviesPage.tsx"))
+const SeriesPage = lazy(() => import("./pages/series/SeriesPage.tsx"))
+const MoviePlayer = lazy(() => import("./pages/movies/MoviePlayer.tsx"))
+const SeriesPlayer = lazy(() => import("./pages/series/SeriesPlayer.tsx"))
+const Register = lazy(() => import("./pages/auth/Register.tsx"))
+const Watchlist = lazy(() => import("./pages/bookmarked/Watchlist.tsx"))
+const ChangePassword = lazy(() => import("./pages/auth/ChangePassword.tsx"))
+const RecoverPasswordByEmail = lazy(() => import("./pages/auth/RecoverPasswordByEmail.tsx"))
+const RecoverPassword = lazy(() => import("./pages/auth/RecoverPassword.tsx"))
+const SearchPage = lazy(() => import("./pages/search/SearchPage.tsx"))
 
 function App() {
     const[sortBy,setSortBy]=useState<string>("popularity.desc")
@@ -31,11 +32,14 @@ function App() {
         queryKey: ['users'],
         queryFn: fetchUser,
     })
-
     const {data:session}=useQuery({
         queryKey: ['session'],
         queryFn:fetchSession,
     })
+    useQuery({
+        queryKey: ['popularData'],
+        queryFn: fetchPopularData,
+    });
 
     if (session === undefined) {
         return <div>Loading...</div>
@@ -43,27 +47,24 @@ function App() {
 
 
     return (
+        <Suspense fallback={<div>Loading Page...</div>}>
             <Routes>
-                <Route path="/" element={session===undefined || session===null ? <MainPage />  : <Navigate to="/home" /> } />
+                <Route path="/" element={session === undefined || session === null ? <MainPage /> : <Navigate to="/home" />} />
                 <Route element={<ProtectedRoute user={user} />}>
-                    <Route path="/home" element={<HomePage />}/>
-                    <Route path="/movies" element={<MoviesPage sortBy={sortBy} setSortBy={setSortBy} releaseDate={releaseDate}
-                                                               setReleaseDate={setReleaseDate} category={category} setCategory={setCategory} genres={genres} setGenres={setGenres} />}/>
-                    <Route path="/series" element={<SeriesPage sortBy={sortBy} setSortBy={setSortBy} releaseDate={releaseDate}
-                                                               setReleaseDate={setReleaseDate} category={category} setCategory={setCategory} genres={genres} setGenres={setGenres}/>}/>
-                    <Route path="/movies/:id/:name" element={<MoviePlayer />}/>
-                    <Route path="/watchlist" element={<Watchlist />}/>
-                    <Route path="/series/:id/:name" element={<SeriesPlayer />}/>
-                    <Route path="/change-password" element={<ChangePassword />}/>
-                    <Route path="/recover-password" element={<RecoverPassword />}/>
-                    <Route path={"/search"} element={<SearchPage/>}/>
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/movies" element={<MoviesPage sortBy={sortBy} setSortBy={setSortBy} releaseDate={releaseDate} setReleaseDate={setReleaseDate} category={category} setCategory={setCategory} genres={genres} setGenres={setGenres} />} />
+                    <Route path="/series" element={<SeriesPage sortBy={sortBy} setSortBy={setSortBy} releaseDate={releaseDate} setReleaseDate={setReleaseDate} category={category} setCategory={setCategory} genres={genres} setGenres={setGenres} />} />
+                    <Route path="/movies/:id/:name" element={<MoviePlayer />} />
+                    <Route path="/watchlist" element={<Watchlist />} />
+                    <Route path="/series/:id/:name" element={<SeriesPlayer />} />
+                    <Route path="/change-password" element={<ChangePassword />} />
+                    <Route path="/recover-password" element={<RecoverPassword />} />
+                    <Route path={"/search"} element={<SearchPage />} />
                 </Route>
-                    <Route path="/recover-password-by-email" element={<RecoverPasswordByEmail />}/>
-                    <Route path="/register" element={<Register />}/>
-
+                <Route path="/recover-password-by-email" element={<RecoverPasswordByEmail />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
-
-
+        </Suspense>
   )
 }
 
